9f3b4038227c35222a6f44b22639d2a535fb15a4
brunosoharagit <b.sohara.git@gmail.com>
2020-05-28 11:53:04 -0300

    adding movements

--- script.js
+++ script.js
@@ -25,6 +25,16 @@ function createSnake()
     }
 }
 
+document.addEventListener("keydown", update)
+
+function update()
+{
+    if(event.keyCode == 37 && direction != "right") direction = "left"
+    if(event.keyCode == 38 && direction != "down") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
 function startGame()
 {
     createBG()
@@ -33,10 +43,10 @@ function startGame()
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
-    if(direction === "right") snake += box
-    if(direction === "left") snakeX -= box
-    if(direction === "up") snakeY += box
-    if(direction === "down") snakeY -= box
+    if(direction == "right") snakeX += box
+    if(direction == "left") snakeX -= box
+    if(direction == "up") snakeY += box
+    if(direction == "down") snakeY -= box
 
     snake.pop()
 
