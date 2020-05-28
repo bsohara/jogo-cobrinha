3716b16d8b9808033e2bcf4eaa7fe612f1c3f73c
brunosoharagit <b.sohara.git@gmail.com>
2020-05-28 11:17:21 -0300

    first commit of files

---
+++ script.js
@@ -0,0 +1,11 @@
+let canvas = document.getElementById("snake")
+let context = canvas.getContext("2d")
+let box = 12
+
+function createBG() 
+{
+    context.fillStyle = "lightgreen"
+    context.fillRect(0, 0, 16 * box, 16 * box)
+}
+
+createBG()
