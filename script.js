aaaebdc22ab766e81afe9fda9c4d5ee774950348
brunosoharagit <b.sohara.git@gmail.com>
2020-05-28 11:39:55 -0300

    creating the function 'start game'

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let directions = "right"
+
 function createBG() 
 {
     context.fillStyle = "lightgreen"
@@ -23,5 +25,29 @@ function createSnake()
     }
 }
 
-createBG()
-createSnake()
+function startGame()
+{
+    createBG()
+    createSnake()
+
+    let snakeX = snake[0].x
+    let snakeY = snake[0].y
+
+    if(direction === "right") snake += box
+    if(direction === "left") snakeX -= box
+    if(direction === "up") snakeY += box
+    if(direction === "down") snakeY -= box
+
+    snake.pop()
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead)
+}
+
+let game = setInterval(startGame, 100)
+
+
