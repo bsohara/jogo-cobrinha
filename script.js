1734605d5750166bc6270e800497719d710a135e
brunosoharagit <b.sohara.git@gmail.com>
2020-05-29 10:44:56 -0300

    update code line Ln3:Col2

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let canvas = document.getElementById("snake")
 let context = canvas.getContext("2d")
-let box = 12
+let box = 32
 let snake = []
 
 snake[0] = {
@@ -37,7 +37,11 @@ function update()
 
 function startGame()
 {
-    if(snake[0].x > 15 * box &&direction == "right") snake[0].x = 0
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16* box
+
     createBG()
     createSnake()
 
