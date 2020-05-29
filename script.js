9bfb48cc528a18d75e02ab999d25f8716851cda7
brunosoharagit <b.sohara.git@gmail.com>
2020-05-29 11:31:48 -0300

    creating food and fix mechanics

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ snake[0] = {
 }
 
 let directions = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function createBG() 
 {
@@ -25,33 +29,41 @@ function createSnake()
     }
 }
 
+function createFood()
+{
+    context.fillStyle = "red"
+    context.fillRect(food.x, food.y, box, box)
+}
+
+
 document.addEventListener("keydown", update)
 
-function update()
+function update(event)
 {
-    if(event.keyCode == 37 && direction != "right") direction = "left"
-    if(event.keyCode == 38 && direction != "down") direction = "up"
-    if(event.keyCode == 39 && direction != "left") direction = "right"
-    if(event.keyCode == 40 && direction != "up") direction = "down"
+    if(event.keyCode == 37 && directions != "right") directions = "left"
+    if(event.keyCode == 38 && directions != "up") directions = "down"
+    if(event.keyCode == 39 && directions != "left") directions = "right"
+    if(event.keyCode == 40 && directions != "down") directions = "up"
 }
 
 function startGame()
 {
-    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0
-    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
-    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
-    if(snake[0].y < 0 && direction == "up") snake[0].y = 16* box
+    if(snake[0].x > 15 * box && directions == "right") snake[0].x = 0
+    if(snake[0].x < 0 && directions == "left") snake[0].x = 16 * box
+    if(snake[0].y < 0 && directions == "down") snake[0].y = 16 * box
+    if(snake[0].y > 15 * box && directions == "up") snake[0].y = 0
 
     createBG()
     createSnake()
+    createFood()
 
     let snakeX = snake[0].x
     let snakeY = snake[0].y
 
-    if(direction == "right") snakeX += box
-    if(direction == "left") snakeX -= box
-    if(direction == "up") snakeY += box
-    if(direction == "down") snakeY -= box
+    if(directions == "right") snakeX += box
+    if(directions == "left") snakeX -= box
+    if(directions == "up") snakeY += box
+    if(directions == "down") snakeY -= box
 
     snake.pop()
 
